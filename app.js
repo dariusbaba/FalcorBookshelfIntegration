5efc492de0555180a8c26c190619861058d7bc93
Darius Baba <darius.baba@intel.com>
2015-10-01 23:18:24 +0300

    promisses fixed by denis

--- app.js
+++ app.js
@@ -1,39 +1,42 @@
 var express = require('express'),
   config = require('./config/config')
 
-var app = express();
+var app = express(),
+    x = require('./app/DB.js');
 
 require('./config/express')(app, config);
 
-var db = require('./app/DB.js')
-db.then(knex => {
-  var db = require('bookshelf')(knex);
-  db.plugin('registry');
-  var factory = require('bookshelf-factory')(config);
-  var type = factory.schemer.constants.type;
-  var schema = {
-    tasks: {
-      id: {
-        type: type.integer,
-        primary: true,
-        increments: true
-      },
-      description: {
-        type: type.string,
-        size: 200
+x.reinitialize()
+  .then(knex => {
+    //this code is ran before the create
+    // ok
+    var db = require('bookshelf')(knex);
+    db.plugin('registry');
+    var factory = require('bookshelf-factory')(config);
+    var type = factory.schemer.constants.type;
+    var schema = {
+      tasks: {
+        id: {
+          type: type.integer,
+          primary: true,
+          increments: true
+        },
+        description: {
+          type: type.string,
+          size: 200
+        }
       }
-    }
-  };
-  // call the create function to create all the models
-  var models = factory.create(schema);
-  console.log(models);
-  // forge the model and get all of its resources
-  models.task.forge().getResources().then(function(results) {
+    };
+    // call the create function to create all the models
+    var models = factory.create(schema);
+    console.log(models);
+    // forge the model and get all of its resources
+    models.tasks.forge().getResources().then(function(results) {
 
-    // pretty print the results to the console
-    console.log(JSON.stringify(results, null, ' '));3
+      // pretty print the results to the console
+      console.log(JSON.stringify(results, null, ' '));3
+    });
   });
-});
 
 app.listen(config.port, function() {
   console.log('Express server listening on port ' + config.port);
--- app/DB.js
+++ app/DB.js
@@ -20,40 +20,58 @@ var tasks = {
 
 var createTables = function() {
   config.connection.database = databaseName
+  //aici il facem din nou..o
   knex = require('knex')(config); // now we connect to the newly created DB
   var Manager = require('knex-schema');
   var manager = new Manager(knex);
   //create/update tables
-  manager.sync([tasks])
-    .then(
-      _ => {
-        console.log('Created tasks table');
-        manager.populate([tasks])
-          .then(_ => console.log('Populated tasks table'))
-      })
+
+  //aci era problema
+  // din create tables nu returnezi promise
+  //
+  return manager.sync([tasks])
+    .then(_ => console.log('db: created tasks table'))
+    .then(_ => manager.populate([tasks]))
+    .then(_ => console.log('Populated tasks table'))
 };
 // manager.reset([articles]); // Remove all data from articles.
 // manager.drop([articles]); // Drop table articles.
 
 var knex = require('knex')(config);
-var dropCommand = 'DROP DATABASE IF EXISTS ' + databaseName
-var rebuildDB = function() {
-    knex.raw(dropCommand)
-    .then(function() {
-      knex.raw('CREATE DATABASE ' + databaseName)
-        .then(function() {
-          console.log('DB CREATED');
-          knex.destroy();
-          createTables()
-        })
-    })
-};
 
 
-module.exports = new Promise((resolve, reject) => {
-  rebuildDB() // I want this shit to be done before i resolve knex
-    .then(_ => {
-      console.log(_);
-      resolve(knex)
-    })
-})
+// ruleaza cu babel nu?
+// poti folosi lambdas?
+// dada...desi am avu cevapb a let
+// lambdas merg..nush daca si let
+function dropDatabase() {
+  var dropCommand = 'DROP DATABASE IF EXISTS ' + databaseName;
+
+  return knex.raw(dropCommand);
+}
+
+function createDatabase() {
+  console.log('db: creating database..');
+  return knex.raw('CREATE DATABASE ' + databaseName);
+}
+
+function destroyConnection() {
+  console.log('db: destroying connection..');
+  return knex.destroy(); // promise? da.. If you ever need to explicitly teardown the connection pool, you may use knex.destroy([callback]). You may use knex.destroy by passing a callback, or by chaining as a promise, just not both.
+}
+
+function reinitialize() {
+  console.log('db: reinitializing..');
+  // asta nu e aceiasi treaba? atata ca nu mai faci un promise explicit nou.? k.ms
+  // ba da e acelasi lucru dar e mai putin cod
+  return dropDatabase()
+    .then(createDatabase)
+    .then(destroyConnection)
+    .then(createTables)
+    .then(_ => console.log('db: rebuildDB... done'))
+    .then(_ => knex);
+}
+
+module.exports = {
+  reinitialize: reinitialize
+}
